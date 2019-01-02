f7b2f3c020765d27e29e125e9198c01a446fde80
duchengyang <duchengyang@siwill.com>
2019-01-02 15:53:32 +0800

    dys跨年

--- data.js
+++ data.js
@@ -14,9 +14,9 @@ var data = {
 
           ],
           "imgs": [
-            // {
-            //       "url": "https://i.loli.net/2018/12/04/5c0643e36130f.png"
-            //   }
+            {
+                  "url": "https://i.loli.net/2019/01/02/5c2c6c34e9aaa.jpg"
+              }
 
           ]
         }
